2cb66cba3afe72fa0efa892e7fbd17714f0f5df7
Faisal <officialfaisal1112@gmail.com>
2023-08-21 18:06:58 +0600

    [DONE] project setup

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+import { errorlogger, logger } from './shared/logger';
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    logger.info('Database connect succussfully');
+    app.listen(() => {
+      logger.info(`Server is listening on ${config.port}`);
+    });
+  } catch (error) {
+    errorlogger.error(error);
+  }
+}
+
+bootstrap();
