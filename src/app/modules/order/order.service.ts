7f1e534088bebd0ab59f83a6ff7158a39c03772b
Faisal <officialfaisal1112@gmail.com>
2023-08-23 12:04:33 +0600

    implement transaction main operation

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -1,15 +1,49 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+import httpStatus from 'http-status';
 import { SortOrder, startSession } from 'mongoose';
+import ApiError from '../../../errors/ApiError';
 import { PaginationHelper } from '../../../helpers/paginationHelper';
 import { IGenericResponse } from '../../../interfaces/common';
 import { IPaginationOptions } from '../../../interfaces/pagination';
+import { Cow } from '../cow/cow.model';
+import { User } from '../user/user.model';
 import { IOrder } from './order.interface';
 import { Order } from './order.model';
 
 const createOrder = async (order: IOrder): Promise<IOrder> => {
+  const cow = await Cow.findById(order.cow);
+  const buyer = await User.findById(order.buyer);
+  const seller = await User.findById(cow?.seller);
+  const cowPrice = cow ? cow.price : 0;
+
+  if (buyer && cow && buyer.budget < cowPrice) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'You have to need more budget to buy this cow!',
+    );
+  }
+
   let allOrdersData;
+
   const session = await startSession();
   try {
     session.startTransaction();
+
+    const newCow = await Cow.findByIdAndUpdate(order.cow, {
+      label: 'sold out',
+    });
+
+    const newBuyerBudget = buyer ? buyer.budget - cowPrice : 0;
+    const deductMoneyFromBuyer = await User.findByIdAndUpdate(order.buyer, {
+      budget: newBuyerBudget,
+    });
+
+    const newSellerIncome = seller ? seller?.income + cowPrice : 0;
+    const addMoneyToSeller = await User.findByIdAndUpdate(cow?.seller, {
+      income: newSellerIncome,
+    });
+
     const newOrder = (
       await (await Order.create(order)).populate('cow')
     ).populate('buyer');
