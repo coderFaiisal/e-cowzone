a5856e85e9dd92402f00aeea0d47748b9aff7e2f
Faisal <officialfaisal1112@gmail.com>
2023-08-25 20:39:28 +0600

    Complete create admin data validation

---
+++ src/app/modules/admin/admin.controller.ts
@@ -0,0 +1,5 @@
+const createAdmin = () => {};
+
+export const AdminController = {
+  createAdmin,
+};
--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AdminController } from './admin.controller';
+import { AdminValidation } from './admin.validation';
 
 const router = express.Router();
 
-router.post('/create-admin');
+router.post(
+  '/create-admin',
+  validateRequest(AdminValidation.createAdminZodSchema),
+  AdminController.createAdmin,
+);
 
 router.post('/login');
 router.get('//my-profile');
