1b1fb725463832ebe7ed05a1e49652a2316ceb94
Faisal <officialfaisal1112@gmail.com>
2023-08-25 21:01:53 +0600

    [DONE] create admin api

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -1,4 +1,19 @@
-const createAdmin = () => {};
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { AdminService } from './admin.service';
+
+const createAdmin = catchAsync(async (req: Request, res: Response) => {
+  const { ...adminData } = req.body;
+  const result = await AdminService.createAdmin(adminData);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Admin created successfully',
+    data: result,
+  });
+});
 
 export const AdminController = {
   createAdmin,
--- src/app/modules/admin/admin.model.ts
+++ src/app/modules/admin/admin.model.ts
@@ -1,15 +1,26 @@
 import { Schema, model } from 'mongoose';
 import { AdminModel, IAdmin } from './admin.interface';
 
-const adminSchema = new Schema<IAdmin, AdminModel>({
-  password: { type: String, required: true },
-  role: { type: String, enum: ['admin'], required: true },
-  name: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+const adminSchema = new Schema<IAdmin, AdminModel>(
+  {
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin'], required: true },
+    name: {
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: true },
+    },
+    phoneNumber: { type: String, required: true, unique: true },
+    address: { type: String, required: true },
   },
-  phoneNumber: { type: String, required: true },
-  address: { type: String, required: true },
-});
+  {
+    timestamps: true,
+  },
+);
+
+adminSchema.statics.isAdminExist = async function (
+  id: string,
+): Promise<Pick<IAdmin, 'role'> | null> {
+  return await Admin.findById(id, { role: 1 }).lean();
+};
 
 export const Admin = model<IAdmin, AdminModel>('Admin', adminSchema);
