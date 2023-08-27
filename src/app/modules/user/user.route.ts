226ddde01bd7976b4a0fffae578a3b8e877202fc
Faisal <officialfaisal1112@gmail.com>
2023-08-27 11:57:39 +0600

    [DONE] user route authorization

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,17 +1,23 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserController } from './user.controller';
 import { UserValidation } from './user.validation';
 
 const router = express.Router();
 
-router.get('/:id', UserController.getSingleUser);
-router.get('/', UserController.getAllUsers);
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleUser);
+
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
+
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(UserValidation.updateUserZodSchema),
   UserController.updateUser,
 );
-router.delete('/:id', UserController.deleteUser);
+
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.deleteUser);
 
 export const UserRoutes = router;
